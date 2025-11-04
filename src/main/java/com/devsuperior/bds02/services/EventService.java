fe6dbc75b92afd3d6affa83085a6baf04a972f68
rafazim <rafael_zim@hotmail.com>
2025-11-04 14:33:36 -0300

    Event Test Passed

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.resources.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    private void copyDtoToEntity(EventDTO dto, Event entity) {
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+
+        City city = new City();
+        city.setId(dto.getCityId());
+        entity.setCity(city);
+    }
+}
--- src/main/java/com/devsuperior/bds02/services/exceptions/DatabaseException.java
+++ src/main/java/com/devsuperior/bds02/services/exceptions/DatabaseException.java
@@ -1,6 +1,5 @@
 package com.devsuperior.bds02.services.exceptions;
 
-@SuppressWarnings("serial")
 public class DatabaseException extends RuntimeException {
     public DatabaseException(String msg) {
         super(msg);
